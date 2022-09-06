c93caefa5423fb71d7153efc8c6f7b6e9c89458a
carlos <soscarlos@gmail.com>
2022-09-06 15:51:19 +0200

    Project set-up: spring boot dependencies and .gitignore completion

---
+++ src/main/java/com/rmc/pejo/PejoApplication.java
@@ -0,0 +1,13 @@
+package com.rmc.pejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PejoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PejoApplication.class, args);
+	}
+
+}
