236c21c4283df2f3dd1d341bb1d827d506d92dc3
mscobblepot <tomecsko.reka@gmail.com>
2022-10-03 14:12:44 +0200

    RMC-60 Removes duplicate footer

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,13 +5,10 @@ import LocationCard from "../components/dashboard/LocationCard";
 import DocumentCard from '../components/dashboard/DocumentCard';
 import TipsCard from '../components/dashboard/TipsCard';
 import ReminderCard from '../components/dashboard/ReminderCard';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
 
 const Home = () => {
     return (
         <>
-        <h1 className='text-center'>Dashboard</h1>
         <Row style={{marginBottom: "2%"}}>
             <Col>
                 <TodoCard />
@@ -28,14 +25,9 @@ const Home = () => {
                 <TipsCard />
             </Col>
             <Col>
-            <ReminderCard />
+                <ReminderCard />
             </Col>
         </Row>
-        <Navbar bg="light" variant="light" className="fixed-bottom">
-            <Container className="justify-content-center">
-                <Navbar.Text>© 2022 Copyright: PeJo</Navbar.Text>
-            </Container>
-      </Navbar>
         </>    
     )
   };
