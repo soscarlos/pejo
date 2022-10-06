b6afa1caa6dd5884e6d32a34275ae58c6fa31ef7
mscobblepot <tomecsko.reka@gmail.com>
2022-10-06 11:16:55 +0200

    Changes design for showing reminders

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import TodoCard from '../../components/dashboard/TodoCard';
 import LocationCard from '../../components/dashboard/LocationCard';
 import DocumentCard from '../../components/dashboard/DocumentCard';
@@ -9,27 +10,27 @@ import './style.css';
 
 const Home = () => {
     return (
-        <>
-        <Row id='firstRow'>
-            <Col className='container'>
-                <TodoCard />
-            </Col>
-            <Col className='container'>
-                <LocationCard />
-            </Col>
-            <Col className='container'>
-                <DocumentCard />
+        <Container fluid>
+            <Row id='firstRow'>
+                <Col className='container'>
+                    <TodoCard />
                 </Col>
-        </Row>
-        <Row id='secondRow'>
-            <Col className='container'>
-                <TipsCard />
-            </Col>
-            <Col className='container'>
-                <ReminderCard />
-            </Col>
-        </Row>
-        </>    
+                <Col className='container'>
+                    <LocationCard />
+                </Col>
+                <Col className='container'>
+                    <DocumentCard />
+                </Col>
+            </Row>
+            <Row id='secondRow'>
+                <Col className='container'>
+                    <TipsCard />
+                </Col>
+                <Col className='container'>
+                    <ReminderCard />
+                </Col>
+            </Row>
+        </Container> 
     )
   };
   
