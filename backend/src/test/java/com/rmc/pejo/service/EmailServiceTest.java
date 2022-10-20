4fd2a299a0018d28a1aa7484efca541c78b43455
carlos <soscarlos@gmail.com>
2022-10-20 16:02:02 +0200

    RMC-70 test service layer email sender dificult to mock

--- backend/src/test/java/com/rmc/pejo/service/EmailServiceTest.java
+++ backend/src/test/java/com/rmc/pejo/service/EmailServiceTest.java
@@ -2,27 +2,36 @@ package com.rmc.pejo.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
 
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
-
+@ExtendWith(MockitoExtension.class)
 class EmailServiceTest {
     @Mock
     JavaMailSender mailSender;
     @InjectMocks
     EmailService service;
-
     MimeMessage testMessage;
 
+    MimeMessageHelper helper;
+
     @BeforeEach
-    void initialize() {
+    void initialize() throws MessagingException {
+        EmailBuilder builder = new EmailBuilder();
         testMessage = mailSender.createMimeMessage();
+        helper = new MimeMessageHelper(testMessage, "utf-8");
+        String email = builder.buildEmail("test name", "link");
+        helper.setText(email, true);
     }
 
     @Test
