4141022149a93df2992014fcff04cb8b44be951c
carlos <soscarlos@gmail.com>
2022-11-10 22:05:46 +0100

    RMC-75 login end point cross origin allowed

--- backend/src/main/java/com/rmc/pejo/endpoints/LoginEndPoint.java
+++ backend/src/main/java/com/rmc/pejo/endpoints/LoginEndPoint.java
@@ -3,6 +3,7 @@ package com.rmc.pejo.endpoints;
 import com.rmc.pejo.service.JwtTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/login")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class LoginEndPoint {
     private final JwtTokenService jwtTokenService;
 
